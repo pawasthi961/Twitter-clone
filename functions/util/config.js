81a51e0e7b8e1ef0347ae0031c0d70d86dc80c9f
Prakhar Awasthi <pawasthi961@gmail.com>
2020-08-09 03:39:45 +0530

    refactoring and organising

---
+++ functions/util/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    apiKey: "AIzaSyBoZlPwdurx8AbKNhIeCcDPchkO0XOtUUA",
+    authDomain: "twitter-clone-3b2ac.firebaseapp.com",
+    databaseURL: "https://twitter-clone-3b2ac.firebaseio.com",
+    projectId: "twitter-clone-3b2ac",
+    storageBucket: "twitter-clone-3b2ac.appspot.com",
+    messagingSenderId: "214744427286",
+    appId: "1:214744427286:web:43fcd9f1da01c566107e37",
+    measurementId: "G-TX96HFPZQ6",
+  };
+
